0b8e8da738b426118ec213a5d415ab3a91c569c1
Netokingpo <ernestozb22@gmail.com>
2024-08-28 19:48:38 -0700

    Set up project with basic configuration and dependencies

---
+++ src/main/java/com/backheroapp/back_hero/BackHeroApplication.java
@@ -0,0 +1,13 @@
+package com.backheroapp.back_hero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackHeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backheroapp/back_hero/BackHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backheroapp.back_hero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
